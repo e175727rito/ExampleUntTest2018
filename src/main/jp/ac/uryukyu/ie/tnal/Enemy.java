1346b47ade0105dd3be1ae00dc96cab1f67e1acd
Rito Mekaru <e175727@ie.u-ryukyu.ac.jp>
2018-11-04 11:17:16 +0900

    ステップ1: 死亡した後で攻撃できてしまう件をどうにかしたい。

--- src/main/jp/ac/uryukyu/ie/tnal/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/tnal/Enemy.java
@@ -48,7 +48,9 @@ public class Enemy {
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+        if(dead == false){
+            hero.wounded(damage);
+        }
     }
 
     /**
